c20e18a662990b34ff45cb7528e59abff7f9c9e9
Henry Pérez <henry.perez2017@uteq.edu.ec>
2021-03-11 17:36:48 -0500

    Implementación de obtención de datos del adapter_revista

--- app/src/main/java/com/example/examenfinal/MainActivity.java
+++ app/src/main/java/com/example/examenfinal/MainActivity.java
@@ -1,18 +1,66 @@
 package com.example.examenfinal;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.examenfinal.Adapter.Adap_Revista;
+import com.example.examenfinal.Modelos.Revista;
+import com.example.examenfinal.WebService.Asynchtask;
 
-    String URL = "";
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
+
+    String URL = "https://revistas.uteq.edu.ec/ws/journals.php";
+    RecyclerView recyclerView;
+    View vista;
+
+    public ArrayList<Revista> revistas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceBundle) {
+        vista = inflater.inflate(R.layout.item_revista, container, false);
+        recyclerView = vista.findViewById(R.id.revista_recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
+        Map<String, String> datos_map = new HashMap<String, String>();
+
+        return vista;
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        JSONObject json_lista = new JSONObject(result);
+        JSONArray json_lista_revistas = json_lista.getJSONArray("");
+
+        revistas = Revista.ObjetoJsonBuild(json_lista_revistas);
+
+        Adap_Revista adatador = new Adap_Revista(getApplicationContext(), revistas);
+
+        recyclerView.setAdapter(adatador);
     }
 }
