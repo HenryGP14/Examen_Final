dfcaa904c7da344c1221b89c394b40ffa0aee925
Henry Pérez <henry.perez2017@uteq.edu.ec>
2021-03-11 16:34:04 -0500

    Implementación de carpeta de WS

---
+++ app/src/main/java/com/example/examenfinal/Adapter/Adap_Revista.java
@@ -0,0 +1,24 @@
+package com.example.examenfinal.Adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adap_Revista extends RecyclerView.Adapter<Adap_Revista> {
+    @NonNull
+    @Override
+    public Adap_Revista onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adap_Revista holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
