d22d7d62b7d558052454732a05892c5cb6612e13
Henry Pérez <henry.perez2017@uteq.edu.ec>
2021-03-11 19:55:06 -0500

    Primera parte de la pregunta finalizada

--- app/src/main/java/com/example/examenfinal/MainActivity.java
+++ app/src/main/java/com/example/examenfinal/MainActivity.java
@@ -32,20 +32,21 @@ public class MainActivity extends AppCompatActivity implements Asynchtask {
         setContentView(R.layout.activity_main);
 
         Map<String, String> datos_map = new HashMap<String, String>();
-
         WebService web = new WebService(URL, datos_map, MainActivity.this, MainActivity.this);
         web.execute("GET");
 
+        // Instanciar el placeHolder con el componente del layout activity_main.xml
         placeHolderView = findViewById(R.id.place_revista);
 
     }
 
     @Override
     public void processFinish(String result) throws JSONException {
+        // Obtencción de datos del web service
         JSONArray json_array = new JSONArray(result);
-
         for (int i = 0; i < json_array.length(); i++){
             JSONObject json_obj_revista = json_array.getJSONObject(i);
+            // Añado el context de la aplicacion y el objeto json para enviarselo al modelo Revista
             placeHolderView.addView(new Revista(getApplicationContext(), json_obj_revista));
         }
     }
--- app/src/main/java/com/example/examenfinal/Modelos/Revista.java
+++ app/src/main/java/com/example/examenfinal/Modelos/Revista.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 @Layout(R.layout.item_revista)
 
 public class Revista {
-
+    // Acciones para cambiar los valores por la API
     @View(R.id.txtTitulo)
     TextView txtTitulo;
 
@@ -29,11 +29,13 @@ public class Revista {
     Context ctx;
     JSONObject obj_revista_json;
 
+    // Constructor que recibira el context de la aplicación y un JSONObjet
     public Revista(Context context, JSONObject item_obj_revista){
         ctx = context;
         obj_revista_json = item_obj_revista;
     }
 
+    // Lectura de la API y cambiandolo al layout creado
     @Resolve
     protected void onResolved(){
         try{
